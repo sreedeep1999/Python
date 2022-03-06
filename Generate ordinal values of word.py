f17f67df7405647421866d7048e4f8537ef245be
P J Sreedeep <95912047+sreedeep1999@users.noreply.github.com>
2022-03-06 20:11:02 +0530

    Create Generate ordinal values of word.py

---
+++ Generate ordinal values of word.py
@@ -0,0 +1,7 @@
+string = "hello world"
+new_string2 = []
+for i in string:
+    new_string2.append(ord(i))
+print("string :", string)
+#print("vowels in string ", new_string )
+print("list of ord values", new_string2)
