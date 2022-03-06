4a1881b4ab5fb6f37cd4f8b2a5dcd7f2cb2d21a3
P J Sreedeep <95912047+sreedeep1999@users.noreply.github.com>
2022-03-06 13:40:25 +0530

    Create Leap year.py

---
+++ Leap year.py
@@ -0,0 +1,7 @@
+print("Leap year between two years")
+starYear=int(input("enter start year"))
+endYear=(int(input("enter last year"))
+print("list of years:")
+for year in range(starYear,endYear):
+    if(0==year%4)and(0!=year%210)or(0==year%400)
+    print(year)
